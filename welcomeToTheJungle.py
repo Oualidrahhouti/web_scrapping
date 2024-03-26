f5fe8394f0ba612afafffd6f02b292c3c212e240
oualid rahhouti <rahhoutiwalid@gmail.com>
2024-03-26 13:53:40 +0100

    storing scrapped data in db

--- welcomeToTheJungle.py
+++ welcomeToTheJungle.py
@@ -1,9 +1,22 @@
 import time
+import mysql.connector
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
+# MySQL database configuration
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'welcome_jungle'
+}
+
+# Connect to MySQL database
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
 
 options = Options()
 options.add_argument("--disable-extensions")
@@ -23,6 +36,17 @@ for job_element in job_elements:
     job_title = job_element.find_element(By.XPATH, ".//h4").text
     company_name = job_element.find_element(By.XPATH, ".//span[@class='sc-ERObt ldmfCZ sc-6i2fyx-3 eijbZE wui-text']").text
     location = job_element.find_element(By.XPATH, ".//span[@class='sc-68sumg-0 gvkFZv']").text
+    
+    # Insert job information into the database
+    insert_query = "INSERT INTO jobs (job_title, company_name, location) VALUES (%s, %s, %s)"
+    insert_values = (job_title, company_name, location)
+    cursor.execute(insert_query, insert_values)
+    conn.commit()
+
     print(f"Job Title: {job_title}\nCompany Name: {company_name}\nLocation: {location}\n")
+
+# Close database connection
+cursor.close()
+conn.close()
 
 driver.quit()
